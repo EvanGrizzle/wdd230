f8595fc3c2f3de1ecb6dbf495c66cbff4ea2eaf8
EvanGrizzle <evangrizzle15@gmail.com>
2022-09-24 13:23:00 -0400

    minor changes

--- scripts/current-year.js
+++ scripts/current-year.js
@@ -1,5 +1,4 @@
 let d = new Date();
 let year = d.getFullYear();
 
-let yearElement = document.getElementById(".current-year")
-document.querySelector(yearElement).textContent = year;
+document.querySelector('#current-year').textContent = " " + year + " ";
